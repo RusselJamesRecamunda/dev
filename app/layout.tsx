407083a5cb85ec53f03015b3974c8764569a5502
Russel_Recamunda <130855654+RusselJamesRecamunda@users.noreply.github.com>
2025-08-23 07:42:18 +0000

    New Practice

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import '@/app/ui/global.css';
+import { Inter, Lusitana } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
